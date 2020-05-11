d01c272b7668d2b9030bbda7b4796b13b890b2af
chandaniprajapati <chandaniprajapati96@gmail.com>
2020-05-11 14:57:07 +0530

    admin update

--- admin/add_product.php
+++ admin/add_product.php
@@ -1,21 +1,30 @@
 <?php 
 include "header.php";
-include "connection.php" 
+include "connection.php";
+session_start();
+if ($_SESSION["admin"] == "") {
+?>
+    <script type="text/javascript">
+        window.location = "admin_login.php";
+    </script>
+<?php
+}
+
 ?>
 
 <div class="main-container container-fluid row p-0 d-flex">
     <div class="col-lg-2 col-md-2 col-2 p-0 bg-dark">
                     <ul class="sidebar">
-                        <li class="d-flex"><i class="fa fa-home text-white p-1" aria-hidden="true"></i> <a href="admin_panel.php" class="text-white">Home</a> </li>
-                        <li class="d-flex"><i class="fa fa-users text-white p-1" aria-hidden="true"></i> <a href="#" class="text-white">Users</a> </li>
-                        <li class="d-flex"><i class="fa fa-plus text-white p-1" aria-hidden="true"></i> <a href="add_product.php" name="addproduct" class="text-white">Add Product</a> </li>
-                        <li class="d-flex"> <i class="fa fa-sign-out" aria-hidden="true"></i> <a href="logout.php" class="text-white">Logout</a> </li>
+                    <li class="d-flex"><i class="fa fa-home text-white p-1" aria-hidden="true"></i> <a href="admin_panel.php" class="text-white">Home</a> </li>
+            <li class="d-flex"><i class="fa fa-plus text-white p-1" aria-hidden="true"></i> <a href="add_product.php" name="addproduct" class="text-white">Add Product</a> </li>
+            <li class="d-flex"><i class="fa fa-users text-white p-1" aria-hidden="true"></i> <a href="admin_panel.php" class="text-white">Users</a> </li>
+            <li class="d-flex"><i class="fa fa-eye text-white p-1" aria-hidden="true"></i> <a href="admin_panel.php" class="text-white">View Products</a> </li>
                     </ul>
     </div>
 
     <div class="col-md-10 col-lg-10 col-2">
         <div class="container-fluid ml-0 border rounded" style="width:800px;">
-                <div class="bg-secondary p-2 text-white"> <h3> <i class="fa fa-plus" aria-hidden="true"></i> Add Product</h3></div>
+                <div class="p-2 text-primary"> <h3> <i class="fa fa-plus" aria-hidden="true"></i> Add Product</h3></div>
                 <hr>
             <form action="" method="post" enctype="multipart/form-data">
                 <div class="form-group">
